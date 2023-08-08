5a89e0467f0346efcd0284357428a188818b711b
gusmgoes <ggoes269@gmail.com>
2023-08-08 13:37:22 -0400

    Testing with Jest v5

--- api-pokemon/src/app.main.spec.ts
+++ api-pokemon/src/app.main.spec.ts
@@ -1,39 +1,19 @@
 /* eslint-disable prettier/prettier */
-import { Test } from '@nestjs/testing';
-import { AppModule } from './app.module';
-import { AppController } from './app.controller';
+import { bootstrap } from './main';
+import { NestFactory } from '@nestjs/core';
 
-describe('Main tests e2e', () => {
-  let appController: AppController;
-  let app: any;
+describe('Main tests', () => {
+  let mockApp: any;
 
   beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-    appController = await moduleRef.resolve(AppController);
-    app = moduleRef.createNestApplication();
-    await app.init();
-    // app.isInitialized = true
-    // How to see if is initialized
-  });
-
-  afterAll(async () => {
-    await app.close();
-  });
-
-  it('should be defined Controller and Service', async () => {
-    const mockedPokemons = {
-      data: { results: [{ id: 1 }, { id: 2 }] },
-      status: 200,
-      statusText: 'OK',
-      headers: {},
-      config: {
-        headers: undefined,
-      },
-      request: {},
+    mockApp = {
+      enableCors: jest.fn(),
+      listen: jest.fn(),
     };
-    jest.spyOn(appController, 'listMany').mockResolvedValue(mockedPokemons);
-    expect(await appController.listMany()).toBe(mockedPokemons);
+    NestFactory.create = jest.fn().mockResolvedValue(mockApp);
+  });
+
+  it('should mock the bootstrap function', async () => {
+    await bootstrap();
   });
 });
